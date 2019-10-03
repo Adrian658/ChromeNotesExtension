e9ebf8bdfc47e4ffadad0131738df73ec96cc628
Adrian Guzman <afg30@case.edu>
2019-10-02 21:52:53 -0400

    Added independent scrollability to notes index container

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.runtime.onInstalled.addListener(function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             //Checks if current page contains specified url
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'},
+                //pageUrl: {hostEquals: 'developer.chrome.com'},
             })],
             //action to take if previous condition checks out
             actions: [new chrome.declarativeContent.ShowPageAction()]
