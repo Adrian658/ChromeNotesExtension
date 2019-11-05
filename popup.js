a1e64f2e7b5484bf3155a299a347bf0e1c134478
Adrian Guzman <afg30@case.edu>
2019-11-04 23:40:22 -0500

    Updated auto save so it saves some time (1.5s) after the last change was made in editor

--- popup.js
+++ popup.js
@@ -1,33 +1,66 @@
+
+var changeCount = 0; //Keeps track of the number of changes made to Quill editor
+
+/*
+ *  Creates the Quill editor
+ */
 function createEditor() {
+
+    //Assign instance of Quill to appropriate HTML section
     var quill = new Quill('.current-note-body', {
         theme: 'snow'
     });
 
-    // Save periodically
+    //Set Quill editor to auto-save after making changes
     var Delta = Quill.import('delta');
-    var change = new Delta();
     quill.on('text-change', function(delta) {
-    change = change.compose(delta);
+
+        $('#autosave-label').text('Saving changes...');
+        changeCount += 1;
+
+        //Save the note if the user does not make changes after current change
+        saveNoteWrapper(changeCount, 1500);
+
     });
 
-    setInterval(function() {
-        if (change.length() > 0) {
-        console.log('Saving changes...', change);
-        $('#autosave-label').text('Saving changes...');
-        var id = $("#current-note-display")[0].getAttribute("data-id");
-        var title = $("#current-note-title").text();
-        var body = Quill.find(document.querySelector("#current-note-body")).getText();
-        var color = "Some random color";
-        //Send entire document
-        editNote(id, title, body, color);
-        $('#autosave-label').text('Autosave completed');
-        change = new Delta();
+}
+
+/*
+ * Initiates a wait function and then checks if the changeCount has been incremented while waiting.
+ * If so, then the user has made changes since the function was called, so no action is taken.
+ * If not, then the user has not made any changes for the timeout value and the current changes are saved.
+ */
+function saveNoteWrapper(changeCount, timeoutValue=3000) {
+    
+    localChangeCount = changeCount;
+
+    setTimeout(function(){
+        if (localChangeCount == changeCount) {
+            console.log("Change ", localChangeCount, " is being saved");
+            saveNote();
+            changeCount = 0;
         }
-    }, 3000);
+    }, timeoutValue);
+}
+
+/*
+ *  Retrieve the body of the Quill editor and update the note's contents in storage
+ */
+function saveNote() {
+
+    //Updated variables of current note
+    var id = $("#current-note-display")[0].getAttribute("data-id");
+    var title = $("#current-note-title").text();
+    var body = Quill.find(document.querySelector("#current-note-body")).getText();
+
+    //Save note with new variables
+    //Ideally we should just be saving the body here
+    editNote(id=id, title=title, body=body);
+    $('#autosave-label').text('Autosave completed');
+
 }
 
 //Executes when the page is loaded
 document.addEventListener("DOMContentLoaded", function(){
-    //chrome.extension.getBackgroundPage().console.log('foo');
     createEditor()
 });
