521ea6f3a689e2d26e2a16aa180b5f3a225ddf26
Adrian Guzman <afg30@case.edu>
2019-11-06 10:31:32 -0500

    Changed formatting of Quill editor and some other UI things.

--- quill-handler.js
+++ quill-handler.js
@@ -6,10 +6,31 @@ var changeCount = 0; //Keeps track of the number of changes made to Quill editor
  */
 function createEditor() {
 
+    var toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+      
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+      
+        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+      
+        ['clean']                                         // remove formatting button
+      ];
+
     //Assign instance of Quill to appropriate HTML section
     var quill = new Quill('#current-note-body', {
         theme: 'snow',
         scrollingContainer: '#scrolling-container',
+        modules: {
+            toolbar: '#toolbar'
+        }
     });
 
     //Set Quill editor to auto-save after making changes
